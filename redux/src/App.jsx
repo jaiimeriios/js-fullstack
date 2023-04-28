9d828f2d9e9dd4c7fe5d5601fba552e2ec0418ea
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2023-04-27 17:27:21 -0700

    Redux files :fireworks:

---
+++ redux/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from './features/counter/Counter';
+// import reactLogo from '/react.svg';
+// import viteLogo from '/vite.svg';
+
+function App() {
+    return (
+        <>
+            <h1>React Redux</h1>
+            <Counter />
+        </>
+    );
+}
+
+export default App;
