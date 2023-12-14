ecdc39d6510399fd6c62ef62e42fd885659002b8
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2023-12-14 11:15:24 -0800

    vite react and react router dom base

---
+++ 04_mern-auth-todos/web/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import Header from './components/Header';
+
+function App() {
+
+
+    return (
+        <>
+            <Header title={'MERN Auth Users Todos'} />
+        </>
+    );
+}
+
+export default App;
---
+++ 04_mern-auth-todos/web/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import reactLogo from '/react.svg';
+import { NavLink } from 'react-router-dom';
+
+const Header = ({ title }) => {
+    // const { logout } = useLogout();
+    // const { userAuth } = useAuthContext();
+
+    // // console.log(userAuth);
+
+    // const handleClick = () => {
+    //     logout();
+    // };
+
+    return (
+        <header>
+            <div>
+                {/* <NavLink to="/" className="home"> */}
+                    <img src={reactLogo} className="logo" alt="logo" />
+                    <h1>{title}</h1>
+                {/* </NavLink> */}
+            </div>
+            {/* <nav>
+                  <NavLink to="/">Home</NavLink> 
+                    {userAuth ? (
+                        <>
+                            <span>{userAuth.email}</span>
+                            <button onClick={handleClick}>logout</button>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink to="/signup">Signup</NavLink>
+                            <NavLink to="/login">login</NavLink>
+                        </>
+                    )}
+                </nav> */}
+        </header>
+    );
+};
+
+export default Header;
